96a31ecc957f2e9c43492e7ba5647779968022e2
Dale Sinnott <froxezaen@gmail.com>
2019-01-21 10:50:20 +0000

    Update ParticlePoolComponent.h
    Added an init function for activation of particles. Fixed inUse error. More init of shape in constructor + new setup func

--- ProjectSkyfire/include/components/ParticlePoolComponent.h
+++ ProjectSkyfire/include/components/ParticlePoolComponent.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////
 //
-// ParticlePoolComponent.h (Data pool object for a particle system)
+// ParticlePoolComponent.h (Data pool object for a particle system, particles are colored cirles)
 // Created by Dale Sinnott
 // 10/01/2019
 //
@@ -19,6 +19,17 @@ class ParticlePoolComponent : public Component
 {
 public:
 	friend class ParticlePool;	
+	void init(sf::Vector2f position, sf::Vector2f velocity, double radius, sf::Color color)
+	{
+		m_position = position;
+		m_velocity = velocity;
+		m_radius = radius;
+		m_color = color;
+		m_inUse = true;
+		setupShape();
+	}
+	
+	bool inUse() { return m_inUse; }
 private:
 	ParticlePool()
 	: m_inUse(false),
@@ -26,17 +37,25 @@ private:
 	  m_velocity(0,0),
 	  m_radius(1),
 	  m_body(m_radius),
-	  m_color(sf::Color(0,0,0)){
+	  m_color(sf::Color(0,0,0))
+	{
+		m_body.setPosition(m_position);
 		m_body.setFillColor(m_color);
 	}
 	
-	bool m_inUse();
-	
+	bool m_inUse;
 	sf::Vector2f m_position;
 	sf::Vector2f m_velocity;
 	sf::CircleShape m_body;
 	double m_radius;
 	sf::Color m_color;
+	
+	void setupShape()
+	{
+		m_body.setPosition(m_position);
+		m_body.setSize(m_radius);
+		m_body.setFillColor(m_color);
+	}
 };
 class ParticlePool
 {
